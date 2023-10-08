ef26b8875310e1554406369653e73efb9076a8e1
Nikhil Jamdar <100251717+nikhiljamdar@users.noreply.github.com>
2023-10-08 10:51:02 +0530

    Add files via upload

---
+++ richtermagnitude.py
@@ -0,0 +1,20 @@
+user=float(input("Enter a value:"))
+
+if user>1.0 and user<=2.0:
+    print("Microearthquakes not felt or rarely felt")
+elif user>=2.0 and user<=4.0:
+    print("Very rarely causes damage")
+elif user>=4.0 and user<=5.0:
+    print("Damage done to weak buildings")
+elif user>=5.0 and user<=6.0:
+    print("Cause damage to the poorly constructed building")
+elif user>=6.0 and user<=7.0:
+    print("Causes damage to well-built structures")
+elif user>=7.0 and user<=8.0:
+    print("Causes damage to most buildings")
+elif user>=8.0 and user<=9.0:
+    print("Causes major destruction")
+elif user>=9.0:
+    print("Causes unbelievable damage")
+else:
+    print("Richter magnitude value is not defined")
